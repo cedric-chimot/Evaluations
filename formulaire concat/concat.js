8b3868e2fa62f08148970b6a159791b1addefad5
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-19 09:49:06 +0200

    autre formule JS pour la concaténation

--- formulaire concat/concat.js
+++ formulaire concat/concat.js
@@ -1,7 +1,11 @@
-function concate() {
+/*function concate() {
     let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     let result = nom.concat(" ", prenom);
     
     document.getElementById("resultat").value = result;
-}
+}*/
+
+concate[2].addEventListener("click", () => {
+    concate[3].value = `${concate[0].value} ${concate[1].value}`
+});
