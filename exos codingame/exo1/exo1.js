21af49740d79fab183ad0a47c793e7cff1c0c817
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-19 13:54:43 +0200

    recherche d'une autre méthode de suppression des doublons

--- exos codingame/exo1/exo1.js
+++ exos codingame/exo1/exo1.js
@@ -14,7 +14,7 @@ for (let x = 0; x < refrain.length; x++) {
     const refrainNumberChildren = Math.round(refrainChildren.length / 2);
 
     for (let y = 0; y < refrainNumberChildren; y += 2) {
-        //let child = refrainChildren[i];
+        // let child = refrainChildren[i];
 
         for (let i = 0; i < 2; i++) {
             refrain[x].removeChild(refrainChildren[y]);
@@ -28,3 +28,33 @@ erreur.remove("erreur");
 const footer = document.createElement("footer");
 footer.innerText = "© Copyright 2020 - Nom";
 document.body.appendChild(footer);
+
+//suppression des doublons avec convertion en tableau
+
+/*var refrain = [
+    "Never gonna give you up",
+    "Never gonna give you up",
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you",
+    "Never gonna tell a lie and hurt you",
+  ];
+  
+  function filterArray(refrain) {
+    var found = {};
+    var out = refrain.filter(function (e) {
+      return found.hasOwnProperty(e) ? false : (found[e] = true);
+    });
+    return out;
+  }
+  
+  const outputArray = filterArray(refrain);
+  console.log("Original Array",refrain);
+  console.log("Filtered Array",outputArray);*/
