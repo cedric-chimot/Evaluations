01aaadc3b3ad3bd97675d2bd4413999440ffd56f
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-20 08:22:56 +0200

    tentatives mouse enter/leave

--- exos codingame/exo3/exo3.js
+++ exos codingame/exo3/exo3.js
@@ -1,20 +1,30 @@
-checkBox = document.getElementById('masquer-paroles').addEventListener('click', event => {
+checkBox = document
+  .getElementById("masquer-paroles")
+  .addEventListener("click", (event) => {
     if (event.target.checked) {
-        $("#paroles").toggle();
-        $('#parole').contents().last().replaceWith(' Afficher les paroles');
+      $("#paroles").toggle();
+      $("#parole").contents().last().replaceWith(" Afficher les paroles");
+    } else {
+      $("#paroles").show();
+      $("#parole").contents().last().replaceWith(" Masquer les paroles");
     }
-    else {         
-        $("#paroles").show();
-        $('#parole').contents().last().replaceWith(' Masquer les paroles');
-    }
-});
+  });
+
 
 checkBox1 = document.getElementById('masquer-refrains').addEventListener('click', event => {
     if (event.target.checked) {
         $(".contenu").hide();
         $(".definition").show();
         $('#refrain').contents().last().replaceWith(' Afficher les refrains');
-               
+        $(document).ready(function() {
+            $(".definition").mouseenter(function() {
+                $(".contenu").show(".contenu");
+            });
+
+            $(".definition").mouseleave(function() {
+                $(".contenu").hide(".contenu");
+            })
+        })
     }
 
     else {        
@@ -24,3 +34,16 @@ checkBox1 = document.getElementById('masquer-refrains').addEventListener('click'
     }
 });
 
+/*$(document).ready(function () {
+            [].forEach.call(document.querySelectorAll(".contenu1"), function () {
+                $(".definition").mouseenter(function () {
+                    $(".contenu1").show(".contenu1");
+            });
+            });
+        
+            [].forEach.call(document.querySelectorAll(".contenu1"), function () {
+                $(".definition").mouseleave(function() {
+                    $(".contenu1").hide(".contenu1");
+                })
+            });
+        });*/    
