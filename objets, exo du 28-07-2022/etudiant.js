167eded04ca0b4d0f2959ac29969cbf1f340ece1
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-29 08:41:05 +0200

    Sauvegarde des fichiers

---
+++ objets, exo du 28-07-2022/etudiant.js
@@ -0,0 +1,3 @@
+const etudiant = {nom: "Chimot", prenom: "Cédric", age: "40"};
+    
+document.getElementById("objet").innerHTML = "L'étudiant s'appelle " + etudiant.prenom + " " + etudiant.nom + ", il a " + etudiant.age + " ans.";
