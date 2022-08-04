e80128693a1e3287c81f6a1c2e84008936801e6c
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-08-04 16:29:41 +0200

    fin de l'exercice ecommerce n°2

--- exos-vacances/Ecommerce2/Assets/ecommerce.js
+++ exos-vacances/Ecommerce2/Assets/ecommerce.js
@@ -1,13 +1,16 @@
+//Création des variables
 const openNav = document.querySelector(".icon1");
 const fermerNav = document.querySelector(".fermer");
 const menu = document.querySelector(".menu");
 
-//"getBoundingClientRect" Renvoie la taille d'un élément et sa position par rapport à la fenêtre
+//Un objet "DOMrect" représente un rectangle
+//"getBoundingClientRect" Retourne un objet"DOMrect" fournissant des infos sur la taille d'un élément et sa position par rapport à la fenêtre
 const positionMenu = menu.getBoundingClientRect().left;
 
-//Création de l'évènement ouvri/fermer pour le menu
+//Création de l'évènement ouvrir/fermer pour le menu
 openNav.addEventListener("click", e => {
     if(positionMenu < 0) {
+        /*Création d'une classe*/
         menu.classList.add("montrer");
     } 
 })
