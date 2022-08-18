28880f181bfbac14492c735a71f0bfa7ac5db2b8
Cedric Chimot <cedric_om10@yahoo.fr>
2022-08-18 16:50:40 +0200

    exo react fromscratch suite

--- cours-react-1/src/App.js
+++ cours-react-1/src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
 const App = () => {
-  return <h1>Hello React</h1>;
-}
+return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      {/* path "*" fonctionne si rien de déclaré au dessus */}
+      <Route path="*" element={<Home/>}/>
+    </Routes>
+  </BrowserRouter>
+);
+};
 
 export default App;
---
+++ cours-react-1/src/pages/About.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A PROPOS</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio illo debitis,
+                dolore dolorem facilis totam minima sed nulla mollitia eveniet provident?
+                Dolorem totam atque adipisci ipsam nobis placeat ea dicta tenetur impedit accusamus quod tempora neque officia rem,
+                omnis tempore est nihil alias quos natus! Cumque est error, mollitia asperiores
+                corporis unde non officia labore, consectetur distinctio nostrum in pariatur harum
+                perferendis dolores omnis amet, ab ipsa dolore. Voluptate nulla, quo perspiciatis optio
+                repellat culpa, perferendis labore nisi, illo enim aperiam vitae ipsam officiis nam?
+                Officia culpa expedita corrupti reiciendis pariatur. Praesentium voluptatibus consequuntur
+                similique eum laudantium error numquam nulla.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio illo debitis,
+                dolore dolorem facilis totam minima sed nulla mollitia eveniet provident?
+                Dolorem totam atque adipisci ipsam nobis placeat ea dicta tenetur impedit accusamus quod tempora neque officia rem,
+                omnis tempore est nihil alias quos natus! Cumque est error, mollitia asperiores
+                corporis unde non officia labore, consectetur distinctio nostrum in pariatur harum
+                perferendis dolores omnis amet, ab ipsa dolore. Voluptate nulla, quo perspiciatis optio
+                repellat culpa, perferendis labore nisi, illo enim aperiam vitae ipsam officiis nam?
+                Officia culpa expedita corrupti reiciendis pariatur. Praesentium voluptatibus consequuntur
+                similique eum laudantium error numquam nulla.</p>
+        </div>
+    );
+};
+
+export default About;
