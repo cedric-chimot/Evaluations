eb4dcb279b6026d508d30c97cdcfe8d62bc714ff
Cedric Chimot <cedric_om10@yahoo.fr>
2022-08-20 14:47:11 +0200

    finalisation de l'exo react 1

--- cours-react-1/src/components/Countries.js
+++ cours-react-1/src/components/Countries.js
@@ -5,7 +5,11 @@ import { useEffect } from 'react';
 import Card from './Card';
 
 const Countries = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
     // Le "useeffect" se joue lorsque le composent est monté
     useEffect(() => {
         axios
@@ -15,11 +19,42 @@ const Countries = () => {
 
     return (
         <div className='countries'>
-            <h1>COUNTRIES</h1>
+            <ul className="radio-container">
+                <input
+                    type="range"
+                    min="1"
+                    max="250"
+                    defaultValue={rangeValue}
+                    onChange={(e) => setRangeValue(e.target.value)}
+                />
+                {radios.map((continent) => (
+                    <li>
+                        <input
+                            type="radio"
+                            id={continent}
+                            name="continentRadio"
+                            checked={continent === selectedRadio}
+                            onChange={(e) => setSelectedRadio(e.target.id)}
+                        />
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
+            </ul>
+
+            {selectedRadio && (
+                    <button onClick={() => setSelectedRadio("")}>
+                        Annuler la recherche
+                    </button>
+            )}
+
             <ul>
                 {
-                    data.map((country, index) => (
-                        <Card key={index} country={country} />
+                    data
+                        .filter((country) => country.continents[0].includes(selectedRadio))
+                        .sort((a, b) => b.population - a.population)
+                        .slice(0, rangeValue)
+                        .map((country, index) => (
+                            <Card key={index} country={country} />
                 ))}
             </ul>
         </div>
